4018f9bba11153412f3eb934088c503d46905cd1
Saransh719 <saranshsapra08@gmail.com>
2025-10-22 03:18:48 +0530

    optimised imports

--- app/src/main/java/com/saransh/whosthatpokemon/MainActivity.kt
+++ app/src/main/java/com/saransh/whosthatpokemon/MainActivity.kt
@@ -6,10 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
